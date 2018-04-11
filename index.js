236795d13b75926fcecbea428c18b75d1ca75679
Superpencil <pascal@superpencil.com>
2018-04-11 15:19:58 +0800

    Rename function to ‘o’

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var React = require('react')
 
 module.exports = o
 
-function r () {
+function o () {
   var argumentsArray = Array.from(arguments)
   var x = argumentsArray[1]
   if (typeof x === 'string' || typeof x === 'number' || Array.isArray(x) || (typeof x === 'object' && Object.keys(x)[0] === '$$typeof')) {
